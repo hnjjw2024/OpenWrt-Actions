8faea9d03ec84480c9dc03dbf972691df6fc5c15
hnjjw2024 <hnjjw@126.com>
2026-05-30 19:08:34 +0800

    Change default IP address in custom startup script

--- build/cmcc_rax3000me/99-custom.sh
+++ build/cmcc_rax3000me/99-custom.sh
@@ -2,7 +2,7 @@
 # 该脚本为immortalwrt首次启动时 运行的脚本 即 /etc/uci-defaults/99-custom.sh 也就是说该文件在路由器内 重启后消失 只运行一次
 # 基础变量
 hostname="immortalwrt"
-ip_address="192.168.1.1"
+ip_address="192.168.10.1"
 netmask="255.255.255.0"
 LOGFILE="/etc/config/uci-defaults-log.txt"
 
